9f4be1085cdd27cc932a0278d69dc5ec745650fa
Carolina Arvidsson <carolinaarvidsson_@live.se>
2020-06-22 21:08:01 +0200

    API

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-window.addEventListener('DOMContentLoaded', () => {             
+window.addEventListener('DOMContentLoaded', () => {      // Fires when the DOM content is loaded, without waiting for images and stylesheets to finish loading
 
   // Global variables
-  const wrapper = document.getElementById('game-wrapper');    
-  const divChests = document.getElementById('chests');        
-  let counter = 0;
+  const wrapper = document.getElementById('game-wrapper');  // Retrieving game-wrapper from HTML
+  const divChests = document.getElementById('chests');      // Retrieving chests from HTML
 
-  let score = 0;                
-  let generatedNumber = null;   
-  let chestOpened = false;     
+  let score = 0;                      // Award points, start from 0
+  let generatedNumber = null;         // Unknown value
+  let chestOpened = false;            // Ensures that you cannot open more than one chest
   
+  // Three objects with value = id
   const firstChest = {
     name: 'first',
     value: 1,
@@ -30,13 +30,15 @@ window.addEventListener('DOMContentLoaded', () => {
     image: 'images/chest-closed.png'
   };
 
-  const chestArray = [firstChest, secondChest, thirdChest];  
+  const chestArray = [firstChest, secondChest, thirdChest];  // Array of the objects
 
+  // Function that initiates the whole Game application
   function init() {
     initRandom();
     initGameUI();
   };
 
+  // Function randomizes a number between 1-3
   function initRandom() {
     generatedNumber = Math.floor(Math.random() * 3 + 1);
   }
@@ -48,6 +50,9 @@ window.addEventListener('DOMContentLoaded', () => {
     initRefreshButton();
   };
 
+  /**
+   * @desc Creating clickable images and give them an id
+   */
   function initChests(){
     chestOpened = false;
 
@@ -61,6 +66,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
 
+  // Creates the scoreboard
   function initScoreBoard() {
     let scoreBoard = document.createElement('div');
     scoreBoard.id = 'score-board';
@@ -76,61 +82,65 @@ window.addEventListener('DOMContentLoaded', () => {
     scoreText.innerText = `Score: ${score}`;
   };
 
+  /**
+   * @desc Updates score by 5 when the chest contains the random number
+   * @param {number} newScore - Sending param updates the scores
+   */
   function changeScore(newScore) {
     score += newScore;
     document.getElementById('score-result').innerText = `Score: ${score}`;
   };
 
+  // Calling the refresh-button
   function initRefreshButton(){
     const refreshBtn = document.getElementById('refresh-button');
     refreshBtn.addEventListener('click', refresh);
   };
 
+  /**
+   * @desc Function handles if or not the chest id matches with the random number
+   * @param {string} e 
+   * @boolean - Closes the other chests after clicking at one
+   */
   function chestClicked(e){
     if (chestOpened === false) {
       chestOpened = true;
-      if (e.target.id === generatedNumber) {
+      if (e.target.id == generatedNumber) {
         e.target.diamond = true;
-        e.target.src = 'images/chest-jewel.png';
+        e.target.src = getImageFromPexels(e);
         changeScore(5);
       } else {
         e.target.src = 'images/chest-open.png';
       }
     }
   };
-
-  /*
-  // make a request towards pexels API and get 1 Diamond image
-  function getImageFromPexels(){
+  /**
+   * @desc Make a request towards pexels API and get 1 Diamond image
+   * @param {string} e 
+   */
+  function getImageFromPexels(e){
     var xhr = new XMLHttpRequest();
-    xhr.open('GET','https://api.pexels.com/v1/search?query=diamonds', true);
-  
-    xhr.onload = function(){
-      if(this.status == 200){
-        console.log("Worked");
-      } else {
-        console.log(this.status);
-      }
-    }
-    xhr.onerror = function(){
-      console.log("Request Error");
-    }
-    
-    // Fake API Key
+    xhr.open('GET', 'https://api.pexels.com/v1/search?query=diamonds+query&per_page1&page=1', true);
     xhr.setRequestHeader('Authorization','563492ad6f917000010000011f096ba6d98b4432b2ba612bc0bc4c25');
+  
+    xhr.addEventListener('load', function() {
+      let imagePexels = JSON.parse(this.response);
+      let randomPic = Math.floor(Math.random() * imagePexels.photos.length) + 1;
+      e.target.src = imagePexels.photos[randomPic].src.small;
+    });
+
     xhr.send();
   };
-  */
 
+  // Calling all the functions needed to start over, refresh
   function refresh(){
     removeChests();
     initChests();
     initRefreshButton();
     initRandom();
-    counter++;
-    console.log(counter);
   };
 
+  // Removes the round of chestgame
   function removeChests(){
     chestArray.forEach(chest => {
       divChests.removeChild(document.getElementById(chest.value));
